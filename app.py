18ecab62a7d1cf498e93382a855d1d229166fd08
Prajwal Chander <123977646+PrajwalChander@users.noreply.github.com>
2023-07-14 13:57:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,65 +14,65 @@ def main():
     st.title("Breast Cancer Prediction")
     c1,c2,c3,c4=st.columns(4)
     with c1:
-        radius_mean=(st.text_input("Enter Radius Mean",value="0"))
+        radius_mean=(st.text_input(" Radius Mean",value="0"))
     with c2:
-        texture_mean = (st.text_input("Enter Texture Mean",value="0"))
+        texture_mean = (st.text_input(" Texture Mean",value="0"))
     with c3:
-        perimeter_mean = (st.text_input("Enter Perimeter Mean",value="0"))
+        perimeter_mean = (st.text_input(" Perimeter Mean",value="0"))
     with c4:
-        area_mean = (st.text_input("Enter Area Mean",value="0"))
+        area_mean = (st.text_input(" Area Mean",value="0"))
     with c1:
-        smoothness_mean = (st.text_input("Enter Smoothness Mean",value="0"))
+        smoothness_mean = (st.text_input(" Smoothness Mean",value="0"))
     with c2:
-        compactness_mean = (st.text_input("Enter Compactness Mean",value="0"))
+        compactness_mean = (st.text_input(" Compactness Mean",value="0"))
     with c3:
-        concavity_mean = (st.text_input("Enter Concavity Mean",value="0"))
+        concavity_mean = (st.text_input(" Concavity Mean",value="0"))
     with c4:
-        concave_pos_mean = (st.text_input("Enter Concavity Pos Mean",value="0"))
+        concave_pos_mean = (st.text_input(" Concavity Pos Mean",value="0"))
     with c1:
-        symmetry_mean= (st.text_input("Enter Symmetry Mean",value="0"))
+        symmetry_mean= (st.text_input(" Symmetry Mean",value="0"))
     with c2:
-        fractal_dimension_mean= (st.text_input("Enter Fractal Dimension Mean",value="0"))
+        fractal_dimension_mean= (st.text_input(" Fractal Dimension Mean",value="0"))
     with c3:
-        radius_se = (st.text_input("Enter Radius SE",value="0"))
+        radius_se = (st.text_input(" Radius SE",value="0"))
     with c4:
-        texture_se = (st.text_input("Enter Texture SE",value="0"))
+        texture_se = (st.text_input(" Texture SE",value="0"))
     with c1:
-        perimeter_se = (st.text_input("Enter Perimeter SE",value="0"))
+        perimeter_se = (st.text_input(" Perimeter SE",value="0"))
     with c2:
-        area_se = (st.text_input("Enter Area SE",value="0"))
+        area_se = (st.text_input(" Area SE",value="0"))
     with c3:
-        smoothness_se = (st.text_input("Enter Smoothness SE",value="0"))
+        smoothness_se = (st.text_input(" Smoothness SE",value="0"))
     with c4:
-        compactness_se = (st.text_input("Enter Compactness SE",value="0"))
+        compactness_se = (st.text_input(" Compactness SE",value="0"))
     with c1:
-        concavity_se = (st.text_input("Enter Concavity SE",value="0"))
+        concavity_se = (st.text_input(" Concavity SE",value="0"))
     with c2:
-        concave_pos_se = (st.text_input("Enter Concave Pos SE",value="0"))
+        concave_pos_se = (st.text_input(" Concave Pos SE",value="0"))
     with c3:
-        symmetry_se = (st.text_input("Enter Symmetry SE",value="0"))
+        symmetry_se = (st.text_input(" Symmetry SE",value="0"))
     with c4:
-        fractal_dimension_se = (st.text_input("Enter Fractal Dimension SE",value="0"))
+        fractal_dimension_se = (st.text_input(" Fractal Dimension SE",value="0"))
     with c1:
-        radius_worst = (st.text_input("Enter Radius Worst",value="0"))
+        radius_worst = (st.text_input(" Radius Worst",value="0"))
     with c2:
-        texture_worst = (st.text_input("Enter Texture Worst",value="0"))
+        texture_worst = (st.text_input(" Texture Worst",value="0"))
     with c3:
-        perimeter_worst = (st.text_input("Enter Perimeter Worst",value="0"))
+        perimeter_worst = (st.text_input(" Perimeter Worst",value="0"))
     with c4:
-        area_worst = (st.text_input("Enter Area Worst",value="0"))
+        area_worst = (st.text_input(" Area Worst",value="0"))
     with c1:
-        smoothness_worst = (st.text_input("Enter Smoothness Worst",value="0"))
+        smoothness_worst = (st.text_input(" Smoothness Worst",value="0"))
     with c2:
-        compactness_worst = (st.text_input("Enter Compactness Worst",value="0"))
+        compactness_worst = (st.text_input(" Compactness Worst",value="0"))
     with c3:
-        concavity_worst = (st.text_input("Enter Concavity Worst",value="0"))
+        concavity_worst = (st.text_input(" Concavity Worst",value="0"))
     with c4:
-        concave_pos_worst = (st.text_input("Enter Concave Pos Worst",value="0"))
+        concave_pos_worst = (st.text_input(" Concave Pos Worst",value="0"))
     with c1:
-        symmetry_worst = (st.text_input("Enter Symmetry Worst",value="0"))
+        symmetry_worst = (st.text_input(" Symmetry Worst",value="0"))
     with c2:
-        fractal_dimension_worst = (st.text_input("Enter Fractal Dimension Worst",value="0"))
+        fractal_dimension_worst = (st.text_input(" Fractal Dimension Worst",value="0"))
     diagnosis=""
     if st.button("Predict"):
         diagnosis=cancer_prediction([float(radius_mean), float(texture_mean), float(perimeter_mean), float(area_mean), float(smoothness_mean),
